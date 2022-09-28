8c8b7ab65b3eba6cc39bbc096ebc4e21560fb697
caseBread <kgu0515@gmail.com>
2022-09-29 03:28:49 +0900

    chore: sample data 추가 및 코드정리

--- src/client/source/components/ProductList.js
+++ src/client/source/components/ProductList.js
@@ -11,6 +11,14 @@ function ProductList() {
       chatCount: 1,
       img: "https://user-images.githubusercontent.com/92029332/192843294-3d3a66b7-c171-4626-9475-b64445aa9447.png",
     },
+    {
+      name: "잎사귀 포스터",
+      address: "역삼동",
+      ago: 3,
+      price: 59000,
+      chatCount: 2,
+      img: "https://user-images.githubusercontent.com/92029332/192860107-f1fed102-8bcf-4517-b2bc-a857bcdd74ff.png",
+    },
   ]; // 임시 데이터
   const list = productNames.map((product, index) => (
     <Product object={product} key={index} /> // 여기서 map사용할 때 key를 넣으래. 형제끼리 구분하기 위함 (https://crong-dev.tistory.com/47)
