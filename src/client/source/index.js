1c8911d86091f669e492b5a0fcb3c38367b4e926
caseBread <kgu0515@gmail.com>
2022-09-28 18:56:08 +0900

    fix: 메인 컴포넌트 변경

--- src/client/source/index.js
+++ src/client/source/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import MainTopBar from "./components/top-bar/main-top-bar";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainTopBar />
   </React.StrictMode>
 );
