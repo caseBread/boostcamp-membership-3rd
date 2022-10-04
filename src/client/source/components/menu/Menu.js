88674ca2f5da3c5db3fe7fb47e92d80a9f6d6b4a
caseBread <kgu0515@gmail.com>
2022-10-04 11:02:18 +0900

    feat: 메뉴 layout 구현

--- src/client/source/components/menu/Menu.js
+++ src/client/source/components/menu/Menu.js
@@ -3,8 +3,16 @@ import "./Menu.scss";
 
 function Menu() {
   return (
-    <div>
+    <div className="menu">
       <TopBar title="메뉴" />
+      <div className="menu-list">
+        <input id="sell-list" type="radio" name="jadsqwo21e" />
+        <label for="sell-list">판매목록</label>
+        <input id="chat-list" type="radio" name="jadsqwo21e" />
+        <label for="chat-list">채팅</label>
+        <input id="like-list" type="radio" name="jadsqwo21e" />
+        <label for="like-list">관심목록</label>
+      </div>
     </div>
   );
 }
