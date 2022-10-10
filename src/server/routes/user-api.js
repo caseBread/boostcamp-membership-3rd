94adcc4cfe5491c0452a3a4f4fb53aab5e31a4cc
caseBread <kgu0515@gmail.com>
2022-10-10 17:40:16 +0900

    chore: 코드 정리

--- src/server/routes/user-api.js
+++ src/server/routes/user-api.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const user = require("../controllers/user.ctrl");
-const { selectUserName } = require("../models/db");
 const router = express.Router();
 
 router.get("/", user.checkUser);
